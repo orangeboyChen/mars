cb6ff26d9eeffa51ba1fedc9847b6dc85bef9b92
orangebchen <orangebchen@tencent.com>
2026-02-02 15:07:59 +0800

    commit

--- mars/build_watch.py
+++ mars/build_watch.py
@@ -52,11 +52,17 @@ def build_watch(tag=''):
     dst_framework_path = INSTALL_PATH + '/device/mars.xcframework'
     dst_framework_device_path = INSTALL_PATH + '/device/mars.framework'
     make_static_framework(libtool_os_dst_lib, dst_framework_device_path, XLOG_COPY_HEADER_FILES, '../')
-    os.system('mv ' + dst_framework_device_path + '/os ' + dst_framework_device_path + '/mars')
+    src = os.path.join(dst_framework_device_path, 'os')
+    dst = os.path.join(dst_framework_device_path, 'mars')
+    if os.path.exists(src):
+        os.rename(src, dst)
 
     dst_framework_simulator_path = INSTALL_PATH + '/simu/mars.framework'
     make_static_framework(libtool_simulator_dst_lib, dst_framework_simulator_path, XLOG_COPY_HEADER_FILES, '../')
-    os.system('mv ' + dst_framework_simulator_path + '/simulator ' + dst_framework_simulator_path + '/mars')
+    src = os.path.join(dst_framework_simulator_path, 'simulator')
+    dst = os.path.join(dst_framework_simulator_path, 'mars')
+    if os.path.exists(src):
+        shutil.move(src, dst)
     os.system('xcodebuild -create-xcframework -framework "' + dst_framework_device_path + '" -framework "' + dst_framework_simulator_path + '" -output "' + dst_framework_path + '"')
 
     print('==================Output========================')
--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,11 +2,11 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "70dff5c7"
+#define MARS_REVISION "6a9cf41d"
 #define MARS_PATH "feature/watchos_xcframework"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2025-04-24 01:52:39"
+#define MARS_BUILD_TIME "2026-02-02 15:07:48"
 #define MARS_TAG ""
-#define MARS_BUILD_TIMESTAMP 1745430759
+#define MARS_BUILD_TIMESTAMP 1770016068
 
 #endif
